5aa4a45ae16efa839fe4adccc2d334d067c1268f
Tobias Armbruster <tobias.armbruster@omikron.net>
2018-01-22 10:07:07 +0100

    add suggest -> search

---
+++ ff-suggest/suggestToSearch.js
@@ -0,0 +1,59 @@
+document.addEventListener("ffReady", function () {
+    var eventAggregator = factfinder.communication.FFCommunicationEventAggregator;
+    var resultDispatcher = factfinder.communication.ResultDispatcher;
+
+    eventAggregator.addBeforeDispatchingCallback(function (event) {
+        if (event.type === "suggest") {
+            console.log(event);
+            /**
+             * Execute a search
+             */
+            eventAggregator.addFFEvent({
+                type: "search",
+                query: event.query,
+                channel: event.channel,
+                "log-internal": "true",
+                topics: function () {
+                    /**
+                     * used below for subscribing
+                     */
+                    return ["searchToSuggest"];
+                }
+            });
+
+            /**
+             * dont execute the suggest request
+             */
+            delete event["type"];
+        }
+    });
+
+    resultDispatcher.subscribe("searchToSuggest", function (result, event) {
+        /**
+         * Transform search -> suggest
+         */
+        var suggestions = searchResultToSuggestions(result.searchResult);
+        /**
+         * Dispatch it to the ff-suggest element
+         */
+        resultDispatcher.dispatchSuggest(suggestions, event);
+    });
+
+    function searchResultToSuggestions(result) {
+        var suggestions = [];
+        result.records.forEach(function (product) {
+            suggestions.push({
+                attributes: {
+                    deeplink: product.record[result.fieldRoles.deeplink],
+                    price: product.record[result.fieldRoles.price]
+                },
+                name: product.record[result.fieldRoles.productName],
+                hitCount: 0,
+                image: product.record[result.fieldRoles.imageUrl],
+                type: "productName"
+            });
+        });
+
+        return suggestions;
+    }
+});
