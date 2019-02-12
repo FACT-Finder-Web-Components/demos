b2f83adf4f3e08985898db56d72ea1df964c5d9d
torsten.knauf <torsten.knauf@omikron.net>
2019-02-12 10:40:41 +0100

    FFWEB-1063 provide demo for scrolling on asn change

---
+++ ff-asn_scrollToTopOnChange/scrollASNOnChange.js
@@ -0,0 +1,5 @@
+document.addEventListener("ffReady", function () {
+    factfinder.communication.ResultDispatcher.subscribe("asn", function (resultData, event) {
+        window.scrollTo(window.scrollX, 0); // or get and scroll a specific scrollbar to a specific position
+    });
+});
