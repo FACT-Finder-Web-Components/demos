270f4b1eadd70f1ad2173fcc5b8137c268c33263
a.laurowski <ext-a.laurowski@omikron.net>
2019-11-27 10:07:08 +0100

    FFWEB-1445: Update demos with newly passed parameters to FFReady (#25)
    * FFWEB-1445: Update demos with newly passed parameters to FFReady

--- ff-asn_scrollToTopOnChange/scrollASNOnChange.js
+++ ff-asn_scrollToTopOnChange/scrollASNOnChange.js
@@ -1,5 +1,6 @@
-document.addEventListener("ffReady", function () {
-    factfinder.communication.ResultDispatcher.subscribe("asn", function (resultData, event) {
+document.addEventListener("ffReady", function (event) {
+    const resultDispatcher = event.resultDispatcher;
+    resultDispatcher.subscribe("asn", function (resultData, event) {
         window.scrollTo(window.scrollX, 0); // or get and scroll a specific scrollbar
     });
 });
