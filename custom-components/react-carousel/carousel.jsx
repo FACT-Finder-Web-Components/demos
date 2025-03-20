44cad8b71af4868d471e1ad5fd5aa45212971055
krzysztofkozlowski <ext-k.kozlowski@fact-finder.com>
2025-03-20 21:16:52 +0100

    migrate React custom carousel demo

--- custom-components/react-carousel/carousel.jsx
+++ custom-components/react-carousel/carousel.jsx
@@ -1,7 +1,12 @@
-const newId = (() => {
-  let uid = 0
-  return () => uid++
-})()
+// <template> tag wrapper component required to overcome React's issue with rendering its content. See https://github.com/facebook/react/issues/19932
+function Template({ children, ...attrs }) {
+    return (
+        <template
+            {...attrs}
+            dangerouslySetInnerHTML={{ __html: children }}
+        />
+    );
+}
 
 function Product(props) {
   const {Deeplink, ImageURL, Price, Title} = props.record
@@ -9,52 +14,35 @@ function Product(props) {
     <a href={Deeplink} className="product">
       <img src={ImageURL} alt={Title} />
       <div className="title">{Title}</div>
-      <div className="price">{Price}</div>
+      <div className="price">{Price} €</div>
     </a>
   )
 }
 
 function Carousel(props) {
-  const ref = React.useRef()
-  const [offset, setOffset] = React.useState(0)
-  const [records, setRecords] = React.useState([])
+  const ref = React.useRef();
+  const [offset, setOffset] = React.useState(0);
+  const [records, setRecords] = React.useState([]);
 
   React.useEffect(() => {
-    const uid = newId()
-    let subscriptionKey = ''
+      (async () => {
+          const {request} = await getFactfinder();
+          const {hits} = await request.search({query: props.query}, {requestOptions: {requestOnly: true}});
+          setRecords(hits.map(h => h.variantValues[0]));
+      })();
+  }, [props.query]);
 
-    withFactfinder().then(factfinder => {
-      const {EventAggregator, ResultDispatcher} = factfinder.communication
-
-      subscriptionKey = ResultDispatcher.subscribe('carousel-' + uid, ({searchResult}) => {
-        setRecords(searchResult.records.map(r => r.record))
-      })
-
-      setTimeout(() => EventAggregator.addFFEvent({
-        type: 'search',
-        query: props.query,
-        topics: () => ['carousel-' + uid]
-      }))
-    })
-
-    return async () => {
-      const {ResultDispatcher} = (await withFactfinder()).communication
-      ResultDispatcher.unsubscribe('carousel-' + uid, subscriptionKey)
-    }
-  }, [props.query])
-
-  const prev = () => setOffset((offset - 4 + records.length) % records.length)
-
-  const next = () => setOffset((offset + 4) % records.length)
+  const prev = () => setOffset((offset - 4 + records.length) % records.length);
+  const next = () => setOffset((offset + 4) % records.length);
 
   const transformStyles = () => {
-    const products = ref.current ? ref.current.querySelectorAll('.product') : []
+    const products = ref.current ? ref.current.querySelectorAll('.product') : [];
     if (products.length) {
-      const offsetPx = products[offset].offsetLeft - products[0].offsetLeft
-      return {transform: 'translateX(-' + offsetPx + 'px)'}
+      const offsetPx = products[offset].offsetLeft - products[0].offsetLeft;
+      return {transform: `translateX(-${offsetPx}px)`};
     }
-    return {}
-  }
+    return {};
+  };
 
   return (
     <div className={'carousel-wrapper ' + props.className} ref={ref}>
@@ -64,35 +52,38 @@ function Carousel(props) {
       </div>
       <div className="button next-button" onClick={next}>❯</div>
     </div>
-  )
+  );
 }
 
-function withFactfinder() {
+function getFactfinder() {
   return new Promise(resolve => {
-    if (typeof window.factfinder !== 'undefined') {
-      resolve(window.factfinder)
+    if (typeof window.factfinder !== `undefined`) {
+      resolve(window.factfinder);
     } else {
-      document.addEventListener('ffReady', event => resolve(event.factfinder))
+      document.addEventListener(`ffReady`, ({factfinder}) => resolve(factfinder));
     }
-  })
+  });
 }
+
 
 function App() {
   return (
     <React.Fragment>
       <Carousel query="hat" className="hats-carousel" />
-      <ff-record-list unresolved>
-        <ff-record>
-          <a data-anchor="{{record.Deeplink}}" className="product">
-            <img alt="{{record.Title}}" data-image="{{record.ImageURL}}" />
-            <div className="title">{"{{record.Title}}"}</div>
-            <div className="price">{"{{record.Price}}"}</div>
-          </a>
-        </ff-record>
+      <ff-record-list>
+        <Template data-role="record">
+          {`<ff-record>
+            <a data-anchor="{{variantValues.0.Deeplink}}" class="product">
+              <img alt="{{variantValues.0.Title}}" data-image="{{variantValues.0.ImageURL}}" />
+              <div class="title">{{variantValues.0.Title}}</div>
+              <div class="price">{{$ variantValues.0.Price}}</div>
+            </a>
+          </ff-record>`}
+        </Template>
       </ff-record-list>
       <Carousel query="shoe" className="shoes-carousel" />
     </React.Fragment>
   )
 }
 
-ReactDOM.render(<App />, document.getElementById('app'))
+ReactDOM.render(<App />, document.getElementById(`app`));
