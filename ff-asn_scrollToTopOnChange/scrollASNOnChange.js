ea04a83f6d00af6b524986dd8dbf7c7691de27ec
fp <42067426+omikron-fp@users.noreply.github.com>
2025-03-31 16:57:20 +0200

    migrate "scroll to top" demo

--- ff-asn_scrollToTopOnChange/scrollASNOnChange.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener("ffReady", function (event) {
-    const resultDispatcher = event.resultDispatcher;
-    resultDispatcher.subscribe("asn", function (resultData, event) {
-        window.scrollTo(window.scrollX, 0); // or get and scroll a specific scrollbar
-    });
-});
