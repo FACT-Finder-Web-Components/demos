915b0f759177ed296b1be61ccea13a93bbd47ff8
krzysztofkozlowski <ext-k.kozlowski@fact-finder.com>
2025-04-27 18:49:52 +0200

    migrate Angular custom carousel demo

--- custom-components/angular-carousel/src/app/app.component.ts
+++ custom-components/angular-carousel/src/app/app.component.ts
@@ -1,27 +1,29 @@
 import { Component } from '@angular/core';
+import {DomSanitizer, SafeHtml} from "@angular/platform-browser";
+
+const recordList = `
+<ff-record-list>
+  <template data-role="record">
+    <ff-record>
+        <a data-anchor="https://www.your.webshop[[variantValues.0.Deeplink]]"
+           data-redirect="https://www.your.webshop[[variantValues.0.Deeplink]]"
+           data-redirect-target="_blank"
+           class="product">
+            <img data-image="[[variantValues.0.ImageURL]]" alt="[[variantValues.0.Title]]">
+            <div class="title">[[variantValues.0.Title]]</div>
+            <div class="price">[[$ variantValues.0.Price]]</div>
+        </a>
+    </ff-record>
+  </template>
+</ff-record-list>
+`;
 
 @Component({
   selector: 'ffc-root',
   template: `
-    <ff-communication url="https://ng-search-web-components.fact-finder.de/fact-finder"
-                      version="ng"
-                      api="v5"
-                      channel="demo-bergfreunde-en"
-                      mustache-delimiters="[[,]]"
-                      default-query="jacket"
-                      search-immediate
-    ></ff-communication>
     <div id="app">
       <ffc-carousel query="hat" class="hats-carousel"></ffc-carousel>
-      <ff-record-list>
-        <ff-record>
-          <a href="[[record.Deeplink]]" class="product">
-            <img data-image="[[record.ImageURL]]" alt="[[record.Title]]">
-            <div class="title">[[record.Title]]</div>
-            <div class="price">[[record.Price]] €</div>
-          </a>
-        </ff-record>
-      </ff-record-list>
+      <div [outerHTML]="recordList"></div>
       <ffc-carousel query="shoe" class="shoes-carousel"></ffc-carousel>
     </div>
   `,
@@ -29,4 +31,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-carousel';
+  recordList: SafeHtml;
+
+  constructor(private sanitizer: DomSanitizer) {
+    this.recordList = this.sanitizer.bypassSecurityTrustHtml(recordList);
+  }
 }
